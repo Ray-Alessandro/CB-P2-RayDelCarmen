62668bf2e2835ff8693a4a46b660023a3613cdb2
Ray Alessandro <ray_punkeki@hotmail.com>
2026-06-18 16:09:56 -0500

    feat: add backend structure

---
+++ CB_BE_RayDelCarmen/src/main/java/com/bakend/CB_BE_RayDelCarmen/CbBeRayDelCarmenApplication.java
@@ -0,0 +1,13 @@
+package com.bakend.CB_BE_RayDelCarmen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbBeRayDelCarmenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbBeRayDelCarmenApplication.class, args);
+	}
+
+}
---
+++ CB_BE_RayDelCarmen/src/test/java/com/bakend/CB_BE_RayDelCarmen/CbBeRayDelCarmenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakend.CB_BE_RayDelCarmen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbBeRayDelCarmenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
